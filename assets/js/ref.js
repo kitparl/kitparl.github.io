7a255a7f52fbfe59aee240db13d650c673f7c46e
kitparl <pranshu.java@gmail.com>
2023-12-22 23:35:24 +0530

    adding real one

---
+++ assets/js/ref.js
@@ -0,0 +1,51 @@
+let courseMonitoryGithub = () => {
+  window.open(
+    'https://github.com/pranshubisht9/nifty-competition-2668',
+    '_blank'
+  );
+
+  }
+
+  let oneStopSolutionsGithub = () => {
+    window.open(
+      'https://github.com/pranshubisht9/One_Stop_Solutions',
+      '_blank'
+    );
+  }
+let KettoGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/ketto.org.git',
+        '_blank'
+      );
+}
+
+let InventoryGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Inventory_Admin_User',
+        '_blank'
+      );
+}
+
+let CodeCovGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/codeCov.io',
+        '_blank'
+      );
+}
+
+let StarWarGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Star_Wars',
+        '_blank'
+      );
+}
+let myResume = () =>{
+  window.open(
+    'https://drive.google.com/file/d/1oQQeG4LNp_mRgWAiAjFMRhlIW3hsMXe2/view?usp=sharing',
+    '_blank'
+  );
+}
+
+GitHubCalendar(".calendar", "pranshubisht9");
+  // or enable responsive functionality
+  GitHubCalendar(".calendar", "pranshubisht9", { responsive: true });
